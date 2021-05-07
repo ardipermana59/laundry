60350760199b09617d2dafff4248b96b5764bfed
ardipermana59 <ardipermana59.com>
2021-05-07 10:56:57 +0700

    ubah factory member

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -7,10 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Member::class, function (Faker $faker) {
 	$gender = ['L','P','L','P'];
+	$provider = rand(1,9);
+	$tlp = '08' . $provider . rand(1,9999999999);
     return [
         'name' => $faker->name,
 		'address' => $faker->address,
 		'gender' => $gender[array_rand($gender)],
-		'tlp' => $faker->phoneNumber,
+		'tlp' => $tlp,
     ];
 });
